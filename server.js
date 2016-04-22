777784b1256ad02c09eb1c9b983c472b16894ff8
Wai-Yin Kwan <waiyin@gophilosophie.com>
2016-04-22 08:16:02 -0700

    4 add home page template

--- server.js
+++ server.js
@@ -1,16 +1,39 @@
 var express = require('express');
+var exphbs = require('express-handlebars');
 var app = express();
 var port = process.env.PORT || 3000;
 
-app.get('/', function(req, res) {
-  res.send('hi')
-})
+// =======================
+// middleware
+// =======================
+
+// set the folder for the views
+app.set('views', './app/views');
+
+// set up templating engine
+app.engine('hbs', exphbs({
+  extname: 'hbs',
+  defaultLayout: 'main',
+  layoutsDir: './app/views/layouts'
+}));
+app.set('view engine', 'hbs');
+
+
+// =======================
+// routes
+// =======================
+
+app.get('/', function (req, res) {
+  res.render('home', { title: 'My Site' });
+});
 
 app.get('/projects', function(req, res) {
   res.send('projects page')
 })
 
-
+// =======================
+// server
+// =======================
 
 app.listen(port, function(){
   console.log('Server is running on port: ' + port)
