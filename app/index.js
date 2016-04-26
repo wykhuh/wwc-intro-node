357b99c4d261a2a6af7cf79f2e29d51cd30c1d34
Wai-Yin Kwan <waiyin@gophilosophie.com>
2016-04-25 23:59:17 -0700

    18 refactor: separate into server and app

---
+++ app/index.js
@@ -0,0 +1,89 @@
+var express = require('express');
+var exphbs = require('express-handlebars');
+var app = express();
+var githubService = require('./services/githubService.js');
+var projectInfoService = require('./services/projectInfoService.js');
+
+var username = 'wykhuh';
+
+// =======================
+// middleware
+// =======================
+
+// set the folder for the views
+app.set('views', './app/views');
+
+// set up templating engine
+app.engine('hbs', exphbs({
+  extname: 'hbs',
+  defaultLayout: 'main',
+  layoutsDir: './app/views/layouts',
+  helpers: {
+    json: function (context) {
+      return JSON.stringify(context);
+    }
+  }
+}));
+app.set('view engine', 'hbs');
+
+// set public static folder
+app.use(express.static('app/public'));
+
+// =======================
+// routes
+// =======================
+
+app.get('/', function (req, res) {
+  var myLinks = [
+    { url: 'http://github.com/wykhuh', text: 'Github' },
+    { url: 'http://nerdycreativity.com', text: 'Blog' },
+    { url: 'https://www.linkedin.com/in/waiyinkwan', text: 'LinkedIn' }
+  ];
+
+  res.render('home', { title: 'My Site', links: myLinks });
+});
+
+app.get('/projects', function (req, res) {
+  githubService.getGithubInfo(username)
+    .then(function (results) {
+      var repos = results.repos;
+      repos.forEach(function (repo, index) {
+        repos[index].hasPost = projectInfoService.fileExists(repo.name);
+      });
+      res.render('projects', {
+        title: 'My Projects',
+        bio: results.bio,
+        repos: results.repos
+      });
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
+});
+
+app.get('/projects/:id', function (req, res) {
+  var currentProjectName = req.params.id;
+
+  projectInfoService.readFile(currentProjectName, function (err, results) {
+    var currentProject = {};
+
+    if (err) {
+      currentProject = {
+        post: currentProjectName + ' is invalid project name.'
+      };
+    } else {
+      currentProject = {
+        name: currentProjectName,
+        post: results,
+        url: 'https://github.com/wykhuh/' + currentProjectName
+      };
+    }
+
+    res.render('project', {
+      title: 'My Project: ' + currentProjectName,
+      project: currentProject
+    });
+  });
+});
+
+module.exports = app;
